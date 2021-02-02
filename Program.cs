56790ff839f38b41bdc40fd79af0261ab03df3dc
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-02-01 18:37:04 -0700

    Add files via upload
    Add project 9

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CS133PROJECT9
+{
+    class Program
+    {
+        private delegate void MyMethodPointer(int a, int b);
+
+        static void Main(string[] args)
+        {
+            MyMethodPointer pointer;
+
+
+            Myclass myclass = new Myclass();
+
+            pointer = myclass.SumOfNumber;
+            pointer += myclass.SubstractionOfNumbers;
+            pointer += myclass.MultiplicationOfNumber;
+            pointer += myclass.DivisionOfNumber;
+
+
+
+            pointer -= myclass.SumOfNumber;
+            pointer -= myclass.SubstractionOfNumbers;
+            
+
+            pointer(8, 2);
+
+        }
+    }
+}
+
+public class Myclass
+{
+    public void SumOfNumber(int parameter1, int parameter2)
+    {
+      Console.WriteLine(parameter1 + parameter2); 
+    }
+
+    public void SubstractionOfNumbers(int x, int y)
+    {
+        Console.WriteLine(x - y);
+    }
+
+    public void MultiplicationOfNumber(int d, int e)
+    {
+        Console.WriteLine(d * e);
+    }
+
+    public void DivisionOfNumber(int d, int e)
+    {
+        Console.WriteLine(d / e);
+    }
+}
